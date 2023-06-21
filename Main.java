6ef50cdac7beb2f0f4bc78affd8fac5baebbdd50
Nick Pruchkovskiy <nick.pruchkovskiy@gmail.com>
2023-06-21 11:51:24 +0300

    pull request changes-2

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main extends Utils{
     public static void main(String[] args) {
         Utils utils = new Utils();
         System.out.println(utils.printIntMethod());
-        System.out.println("~~~ <повернене значення> ~~~ \n" + someNumber);
+        System.out.println("~~~" +someNumber+ "~~~");
     }
 }
--- Utils.java
+++ Utils.java
@@ -1,7 +1,7 @@
 public class Utils {
     protected static int someNumber = 7;
     protected static int printIntMethod() {
-        System.out.println("---<значення поля>---");
+        System.out.println("---<" +someNumber+ ">---");
         return someNumber;
     }
 }
